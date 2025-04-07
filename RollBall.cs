7b3f6f636b2ca9d59ff11ee982ac393520a08124
SergiDevi <135206847+SergiDevi@users.noreply.github.com>
2025-04-08 00:22:23 +0200

    Update RollBall.cs

--- RollBall.cs
+++ RollBall.cs
@@ -6,22 +6,18 @@ public class RollBall : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
-
-    // Se llama antes de que empiece el primer frame
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Se llama una vez por cada frame fijo de física
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical   = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-
-        // Se multiplica también por Time.fixedDeltaTime para un movimiento más suave
         rb.AddForce(movement * ( speed * Time.fixedDeltaTime) );
     }
 }
