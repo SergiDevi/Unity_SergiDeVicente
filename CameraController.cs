824931515971a3f59b66a1b908002d5e06e9e715
SergiDevi <135206847+SergiDevi@users.noreply.github.com>
2025-04-08 00:21:56 +0200

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -6,15 +6,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
-
-    // Se llama antes de que empiece el primer frame
+    private Vector3 offset;    
     private void Start()
     {
         offset = transform.position - player.transform.position;
     }
-
-    // Se llama después de que la cámara y los objetos se hayan actualizado
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
