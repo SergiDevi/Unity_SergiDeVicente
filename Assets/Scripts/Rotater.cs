84b0526da24d543455a0cb2ead3bee66a84cc325
SergiDevi <sergi.devicente02@estudiant.upf.edu>
2025-04-08 12:00:32 +0200

    Final corrected version

--- Assets/Scripts/Rotater.cs
+++ Assets/Scripts/Rotater.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
-{ 
+public class Rotater : MonoBehaviour
+{
+
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
     }
 }
